ba41fb56f3f9e87ce81ce60fa3cdbd5e8ed021fa
qwekertarisonov-eng <qwekertarsionov@gmail.com>
2026-08-07 20:15:47 +0300

    build: move index.js to root folder and force update npm link

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -3,7 +3,8 @@ import fs from 'fs/promises';
 import path from 'path';
 import os from 'os';
 import nock from 'nock';
-import pageLoader from '../src/index.js';
+import pageLoader from '../index.js';
+
 
 nock.disableNetConnect();
 
